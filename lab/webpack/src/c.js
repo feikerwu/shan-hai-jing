fb25bf1a78f7d80af72b1adcd5d314026a8fcdb3
feikerwu <feikerwu@gmail.com>
2020-06-30 18:00:46 +0800

    chore: code waterfall

---
+++ lab/webpack/src/c.js
@@ -0,0 +1,4 @@
+import b from './b';
+export let c1 = '我是c111';
+export let c2 = '我是c222';
+export default '我是c';
---
+++ lab/webpack/src/entry.js
@@ -0,0 +1,4 @@
+import c from './c';
+import { c1, c2 } from './c';
+console.log(c, c1, c2);
+export default '我是a';
