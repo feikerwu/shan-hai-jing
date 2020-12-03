e1416d31877ce3bb349c20a11ccaeb7879f03046
feikerwu <feikerwu@gmail.com>
2020-12-03 11:37:53 +0800

    feat: add baidu tongji

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,12 +53,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `G-LZLMLK3Q0Jyd`,
+    //   },
+    // },
     `gatsby-plugin-feed`,
     `gatsby-plugin-sass`,
     {
