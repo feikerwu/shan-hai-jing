2192b401c98e168efa08e7283de908202ec02b1a
feiker <feikerwu@gmail.com>
2021-05-24 23:16:56 +0800

    chore: 修改文件名大小写的

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 
 import Bio from '../components/bio';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const BlogIndex = ({ data, location }) => {
