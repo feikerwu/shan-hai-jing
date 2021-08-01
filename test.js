1e78334c15409926704017fac222c666375eb596
feiker <feikerwu@gmail.com>
2021-08-02 00:04:11 +0800

    feat: 08.01

--- test.js
+++ test.js
@@ -1,26 +1,35 @@
 /**
- * @param {string} text1
- * @param {string} text2
- * @return {number}
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
  */
-var longestCommonSubsequence = function(text1, text2) {
-  let dp = Array.from({length: text1.length + 1})
-  .map(_ => Array.from({length: text2.length + 1}).fill(0))
+var kWeakestRows = function (mat, k) {
+  function find(nums) {
+    let start = 0, end = nums.length - 1;
 
-  for (var i = 1; i <= text1.length; i++) {
-    for (let j = 1; j <= text2.length; j++) {
-      if (text1[i - 1] === text2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1
+    while(start <= end) {
+      let mid = Math.floor((start + end) / 2)
+      if (nums[mid] === 1) {
+        start = mid + 1
       } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+        end = mid - 1
       }
     }
+
+    return start
   }
 
-  // console.log(dp)
+  let arr = [];
+  for (let col of mat) {
+    let count = find(col)
+    arr.push(count);
+  }
 
-  return dp[text1.length][text2.length]
+  arr = arr.map((v, i) => ({v, i}));
+
+  console.log(arr)
+
+  return arr.sort((a, b) => a.v - b.v).map(item => item.i).slice(0, k)
 };
 
-// console.log(longestCommonSubsequence("abc",
-// "def"))
+console.log(kWeakestRows([[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]], 3))
