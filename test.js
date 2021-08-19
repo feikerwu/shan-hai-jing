737d7e619a37c88be1216d54eecc4ca05b49b8d0
feikerwu <feikerwu@gmail.com>
2021-08-19 11:48:47 +0800

    feat: daily update

--- test.js
+++ test.js
@@ -1,47 +1,34 @@
 /**
- * @param {number} n
- * @return {number}
+ * @param {string} s
+ * @return {string}
  */
-var checkRecord = function (n) {
-  const mod = 10 ** 9 + 7;
-  let dp = Array.from({ length: n + 1 }).map((_) =>
-    Array.from({ length: 2 }).map((_) => Array.from({ length: 3 }).fill(0))
-  );
+var reverseVowels = function (s) {
+  s = s.split('');
 
-  dp[0][0][0] = 1;
+  let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
 
-  for (let i = 1; i <= n; i++) {
-    // 前一个序列结尾补P
-    for (let j = 0; j < 2; j++) {
-      for (let k = 0; k < 3; k++) {
-        dp[i][j][0] = (dp[i][j][0] + dp[i - 1][j][k]) % mod;
-      }
+  let start = 0,
+    end = s.length - 1;
+
+  while (start < end) {
+    while (start < end && !vowels.has(s[start])) {
+      start++;
     }
 
-    // 前一个序列结尾补 A
-    for (let k = 0; k < 3; k++) {
-      dp[i][1][0] = (dp[i][1][0] + dp[i - 1][0][k]) % mod;
+    while (start < end && !vowels.has(s[end])) {
+      end--;
     }
 
-    // 前一个序列结尾补 L
-    for (let j = 0; j < 2; j++) {
-      for (let k = 1; k < 3; k++) {
-        dp[i][j][k] = (dp[i][j][k] + dp[i - 1][j][k - 1]) % mod;
-      }
-    }
+    swap(s, start++, end--);
   }
 
-  // sum
-  let sum = 0;
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 3; j++) {
-      sum = (sum + dp[n][i][j]) % mod;
-    }
-  }
-
-  console.log({ sum });
-  return sum;
+  return s.join('');
 };
 
-checkRecord(1);
-checkRecord(2);
+function swap(s, a, b) {
+  let term = s[a];
+  s[a] = s[b];
+  s[b] = term;
+}
+
+console.log(reverseVowels('hello'));
