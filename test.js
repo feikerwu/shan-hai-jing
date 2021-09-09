83b59ff186a4a8e5d648b222867817b3ceb82579
feikerwu <feikerwu@gmail.com>
2021-09-09 16:14:09 +0800

    feat: 09.09

--- test.js
+++ test.js
@@ -1,15 +1,48 @@
 /**
- * @param {string} s
- * @return {number}
+ * @param {number} n
+ * @param {number[]} blacklist
  */
-var balancedStringSplit = function (s) {
-  let prefix = 0,
-    count = 0;
+var Solution = function(n, blacklist) {
+  const len = blacklist.length
+  const limit = n - len
+  const blackSet = new Set(blacklist)
 
-  for (let char of s) {
-    prefix += char === 'L' ? 1 : -1;
-    count += prefix === 0;
+  let term = []
+  for (let i = limit; i < n; i++) {
+    if (blackSet.has(i)) {
+      blackSet.delete(i)
+    } else {
+      term.push(i)
+    }
   }
 
-  return count;
+  let m = {}, j = 0;
+
+  for (let v of blackSet) {
+    if (v < limit) {
+      m[v] = term[j++]
+    }
+  }
+
+  this.limit = limit
+  this.map = m
+  this.blackSet = blackSet
 };
+
+/**
+ * @return {number}
+ */
+Solution.prototype.pick = function() {
+  const { limit, map, blackSet } = this
+
+  const randomValue = Math.floor(Math.random() * limit)
+
+  return blackSet.has(randomValue) ? map[randomValue] : randomValue
+
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(n, blacklist)
+ * var param_1 = obj.pick()
+ */
