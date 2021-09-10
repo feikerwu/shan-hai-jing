46082acc60b6a0b5f9620938dae9d6fd927fa43e
feikerwu <feikerwu@gmail.com>
2021-09-10 11:10:07 +0800

    feat: 09-10 daily

--- test.js
+++ test.js
@@ -1,48 +1,20 @@
 /**
- * @param {number} n
- * @param {number[]} blacklist
- */
-var Solution = function(n, blacklist) {
-  const len = blacklist.length
-  const limit = n - len
-  const blackSet = new Set(blacklist)
-
-  let term = []
-  for (let i = limit; i < n; i++) {
-    if (blackSet.has(i)) {
-      blackSet.delete(i)
-    } else {
-      term.push(i)
-    }
-  }
-
-  let m = {}, j = 0;
-
-  for (let v of blackSet) {
-    if (v < limit) {
-      m[v] = term[j++]
-    }
-  }
-
-  this.limit = limit
-  this.map = m
-  this.blackSet = blackSet
-};
-
-/**
+ * @param {number[]} chalk
+ * @param {number} k
  * @return {number}
  */
-Solution.prototype.pick = function() {
-  const { limit, map, blackSet } = this
+var chalkReplacer = function(chalk, k) {
+  let preSum = 0
+  for (let i = 0; i < chalk.length; i++) {
+    preSum += chalk[i]
+    chalk[i] = preSum
+  }
 
-  const randomValue = Math.floor(Math.random() * limit)
+  k = k % preSum
 
-  return blackSet.has(randomValue) ? map[randomValue] : randomValue
-
+  for (let i = 0; i < chalk.length; i++) {
+    if (chalk[i] >= k) {
+      return i
+    }
+  }
 };
-
-/**
- * Your Solution object will be instantiated and called as such:
- * var obj = new Solution(n, blacklist)
- * var param_1 = obj.pick()
- */
