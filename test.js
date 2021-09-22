c66e526712f6db1acd821631eec6b8ca143c82eb
feikerwu <feikerwu@gmail.com>
2021-09-22 14:45:31 +0800

    feat: daily 09.22

--- test.js
+++ test.js
@@ -1,68 +1,59 @@
 /**
- * @param {character[][]} board
- * @param {string} word
- * @return {boolean}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var exist = function (board, word) {
-  const m = board.length,
-    n = board[0].length;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (board[i][j] === word[0]) {
-        let set = new Set();
-        if (backtrack(board, i, j, word, 0, set)) {
-          return true;
-        }
-      }
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
+
+var splitListToParts = function (head, k) {
+  const len = getLength(head);
+  const subListLen = Math.floor(len / k);
+  const remain = len % k;
+
+  let res = [];
+
+  for (let i = 0; i < k; i++) {
+    let count = i < remain ? subListLen + 1 : subListLen,
+      pre = head,
+      dummy = head;
+
+    while (count-- && head) {
+      pre = head;
+      head = head.next;
     }
+    if (pre) {
+      pre.next = null;
+    }
+    res.push(dummy);
   }
-  return false;
+
+  return res;
 };
 
-function backtrack(board, x, y, target, index, visited) {
-  // 递归出口
-  if (index === target.length) {
-    return true;
+function getLength(head) {
+  let len = 0;
+  while (head) {
+    head = head.next;
+    len++;
   }
-
-  // 是否越界
-  if (x < 0 || y < 0 || x >= board.length || y >= board[0].length) {
-    return false;
-  }
-
-  // 是否相等
-  if (board[x][y] !== target[index]) {
-    return false;
-  }
-
-  // 判断是否已访问
-  if (visited.has(`${x},${y}`)) {
-    return false;
-  }
-
-  // 如果满足条件，将其加入到已遍历
-  visited.add(`${x},${y}`);
-
-  // 尝试往4个方向
-  let flag =
-    backtrack(board, x + 1, y, target, index + 1, visited) ||
-    backtrack(board, x, y + 1, target, index + 1, visited) ||
-    backtrack(board, x - 1, y, target, index + 1, visited) ||
-    backtrack(board, x, y - 1, target, index + 1, visited);
-
-  // 回溯
-  visited.delete(`${x},${y}`);
-
-  return flag;
+  return len;
 }
 
-console.log(
-  exist(
-    [
-      ['A', 'B', 'C', 'E'],
-      ['S', 'F', 'E', 'S'],
-      ['A', 'D', 'E', 'E'],
-    ],
-    'ABCESEEEFS'
-  )
-);
+function getTop(head, count) {
+  let dummy = head,
+    pre = head;
+  while (count-- && head) {
+    pre = head;
+    head = head.next;
+  }
+
+  pre.next = null;
+
+  return { dummy, newHead: head };
+}
