7bf0239312613924625cb7ee99b49ffc74ce26ca
feikerwu <feikerwu@gmail.com>
2022-01-25 15:10:40 +0800

    chore: deploy

--- src/components/TagSearcher/index.tsx
+++ src/components/TagSearcher/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 export default () => {
-  const data = useStaticQuery(graphql``);
+  // const data = useStaticQuery(graphql``);
   return <div></div>;
 };
