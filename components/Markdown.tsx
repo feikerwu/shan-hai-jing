4bf55e2e1b8c982424b939413d9102874ea7dce9
feikerwu <feikerwu@gmail.com>
2022-07-12 17:24:10 +0800

    chore: next/image 大小写敏感

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,5 +1,5 @@
 import ReactMarkdown from 'react-markdown';
-import Image from 'next/Image';
+import Image from 'next/image';
 import remarkGfm from 'remark-gfm';
 
 const MarkdownComponent: React.FC<{ content: string }> = ({ content }) => {
