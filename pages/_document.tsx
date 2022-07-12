e2f1f280e74a1cc91fa64fe5f0c93353ee8174b7
feikerwu <feikerwu@gmail.com>
2022-07-12 18:41:38 +0800

    chore: add track

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,12 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter&display=optional'
             rel='stylesheet'
           />
+          <script
+            async
+            defer
+            data-website-id='8371f278-5ff1-45ce-aac2-2f2d64b975d0'
+            src='https://umami-production-a72b.up.railway.app/umami.js'
+          ></script>
         </Head>
         <body>
           <Main />
