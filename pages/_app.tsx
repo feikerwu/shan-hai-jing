3cdbb6bc0f4d8561712b7429c1f50ee627d8e109
feikerwu <feikerwu@gmail.com>
2022-07-13 11:30:06 +0800

    feat: 代码高亮

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import '../styles/globals.css';
+import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'components/Layout';
 import hljs from 'highlight.js';
-import 'highlight.js/styles/mono-blue.css';
+// import 'highlight.js/styles/nnfx-light.css';
+import 'highlight.js/styles/atom-one-light.css';
 import { useLayoutEffect } from 'react';
+
+import 'styles/custom.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useLayoutEffect(() => {
