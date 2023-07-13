f3a6ff4fa79bebd36ae05c98f651ac4182ceca07
feikerwu <feikerwu@gmail.com>
2023-07-13 16:02:34 +0800

    feat: 修改post的样式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={'prose lg:prose-xl'}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import { allPosts } from 'contentlayer/generated';
+import { allPosts, Post } from 'contentlayer/generated';
+import MDXContent from '@/app/components/mdx-content';
 
 // 生成动态路由头部的 slug 信息
 export function generateStaticParams() {
@@ -15,10 +16,15 @@ export default function PostPage({
 }: {
   params: PostPageParams;
 }) {
-  console.log(params);
-  console.log(rest);
+  console.log({ params });
+  console.log({ rest });
 
   const { slug } = params;
+  const post = allPosts.find(post => post._raw.flattenedPath === slug) as Post;
 
-  return <div>{slug}</div>;
+  return (
+    <article className={'prose lg:prose-xl m-auto'}>
+      <MDXContent code={post?.body.code}></MDXContent>
+    </article>
+  );
 }
