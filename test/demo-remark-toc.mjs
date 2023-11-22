224ac27b3e3c8c9d0e3838579a135b1da24a2de9
feiker <feikerwu@gmail.com>
2023-11-22 16:15:48 +0800

    chore: 移除不需要的文件

--- test/demo-remark-toc.mjs
+++ test/demo-remark-toc.mjs
@@ -4,8 +4,6 @@ import remarkFrontmatter from 'remark-frontmatter';
 
 const content = fs.readFileSync('./blog/example.md', 'utf-8');
 
-// console.log('pre', content);
-
 async function main() {
   const file = await remark().use(remarkFrontmatter).process(content);
   console.log(String(file));
